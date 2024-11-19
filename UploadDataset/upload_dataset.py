d477ea2fbf3deb612f03f278b3f56943d9d443e2
AlirezaF80 <alirezafarzaneh138@gmail.com>
2024-11-19 22:50:34 +0330

    Update dataset uploading to handle multiple PES versions

--- UploadDataset/upload_dataset.py
+++ UploadDataset/upload_dataset.py
@@ -10,24 +10,36 @@ import glob
 HF_TOKEN = os.getenv("HF_TOKEN")
 HF_REPO_NAME = "AlirezaF138/PES-Face-Maker"
 
-PES_VERSION = "pes21_dt36"
-EMBEDDINGS_PATH = f"D:/Projects/Pycharm Projects/PES-Face-Maker/embeddings/{PES_VERSION}"
-PCA_OUTPUTS_PATH = f"D:/Projects/Pycharm Projects/PES-Face-Maker/pca_outputs/{PES_VERSION}"
+BASE_PATH = "D:/Projects/Pycharm Projects/PES-Face-Maker"
 
-embedding_files = glob.glob(os.path.join(EMBEDDINGS_PATH, "*.npy"))
-embedding_ids = set(map(lambda x: os.path.basename(x).replace('.npy', ''), embedding_files))
+PES_VERSIONS = [
+    "pes21_dt36",
+    "pes21_dlc1",
+    "pes21_dlc2",
+    "pes21_dlc3",
+    "pes21_dlc4",
+    "pes21_dlc5",
+    "pes21_dlc6",
+    "pes21_dlc7"
+]
 
-pca_files = glob.glob(os.path.join(PCA_OUTPUTS_PATH, "*.npy"))
-pca_ids = set(map(lambda x: os.path.basename(x).replace('.npy', ''), pca_files))
-
-common_ids = embedding_ids.intersection(pca_ids)
-
-# Each row should be a face embedding, a PCA reduced embedding, and the face id, and also the face model used, and pes version
 data = []
-for face_id in common_ids:
-    embedding = list(np.load(os.path.join(EMBEDDINGS_PATH, f"{face_id}.npy")))
-    pca_embedding = list(np.load(os.path.join(PCA_OUTPUTS_PATH, f"{face_id}.npy")))
-    data.append([embedding, pca_embedding, face_id, "Facenet", PES_VERSION])
+for pes_version in PES_VERSIONS:
+    EMBEDDINGS_PATH = f"{BASE_PATH}/embeddings/{pes_version}/"
+    PCA_OUTPUTS_PATH = f"{BASE_PATH}/pca_outputs/{pes_version}/"
+
+    embedding_files = glob.glob(os.path.join(EMBEDDINGS_PATH, "*.npy"))
+    embedding_ids = set(map(lambda x: os.path.basename(x).replace('.npy', ''), embedding_files))
+
+    pca_files = glob.glob(os.path.join(PCA_OUTPUTS_PATH, "*.npy"))
+    pca_ids = set(map(lambda x: os.path.basename(x).replace('.npy', ''), pca_files))
+
+    common_ids = embedding_ids.intersection(pca_ids)
+
+    for face_id in common_ids:
+        embedding = list(np.load(os.path.join(EMBEDDINGS_PATH, f"{face_id}.npy")))
+        pca_embedding = list(np.load(os.path.join(PCA_OUTPUTS_PATH, f"{face_id}.npy")))
+        data.append([embedding, pca_embedding, face_id, "Facenet512", pes_version])
 
 df = pd.DataFrame(data, columns=["embedding", "pca_embedding", "face_id", "model_name", "pes_version"])
 dataset = datasets.Dataset.from_pandas(df)
