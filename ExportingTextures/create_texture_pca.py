ea8b4de2cba4a26dbab16286a693418d4f16a3d2
AlirezaF80 <alirezafarzaneh138@gmail.com>
2024-11-21 03:09:49 +0330

    Save texture PCA to correct folder

--- ExportingTextures/create_texture_pca.py
+++ ExportingTextures/create_texture_pca.py
@@ -9,6 +9,7 @@ import time
 PES_VERSIONS = ["pes21_dt36", "pes21_dlc1", "pes21_dlc2", "pes21_dlc3", "pes21_dlc4", "pes21_dlc5", "pes21_dlc6", "pes21_dlc7"]
 
 BASE_PATH = "D:/Projects/Pycharm Projects/PES-Face-Maker"
+EXPORTING_TEXTURES_FOLDER_PATH = os.path.join(BASE_PATH, "ExportingTextures")
 TEXTURES_PATH = os.path.join(BASE_PATH, "texture_exports")
 
 texture_paths = []
@@ -41,7 +42,9 @@ all_textures = np.array(all_textures)
 print("All textures loaded and PCA trained.")
 
 # Save PCA model
-np.save(os.path.join(BASE_PATH, "pca_texture_model.npy"), pca)
+np.save(os.path.join(EXPORTING_TEXTURES_FOLDER_PATH, "pca_texture_model.npy"), pca)
+
+print("PCA model saved.")
 
 # Transform textures and reconstruct some for error analysis
 pca_start_time = time.time()
@@ -50,11 +53,8 @@ compressed_textures = pca.transform(all_textures[:n_reconstructions])
 reconstructed_textures = pca.inverse_transform(compressed_textures)
 print(f"PCA and reconstruction completed in {time.time() - pca_start_time:.2f} seconds.")
 
-# Save reconstructed textures and calculate errors
-os.makedirs(os.path.join(BASE_PATH, "reconstructed_textures"), exist_ok=True)
+# Calculate errors
 reconstructed_textures = reconstructed_textures.reshape(n_reconstructions, TEXTURE_SIZE, TEXTURE_SIZE, 3)
 for i, texture in enumerate(reconstructed_textures):
-    texture_path = os.path.join(BASE_PATH, "reconstructed_textures", f"reconstructed_texture_{i}.png")
-    cv2.imwrite(texture_path, texture.astype(np.uint8))
     error = np.mean((all_textures[i] - reconstructed_textures[i].reshape(-1)) ** 2)
     print(f"Reconstruction {i} error: {error}")
